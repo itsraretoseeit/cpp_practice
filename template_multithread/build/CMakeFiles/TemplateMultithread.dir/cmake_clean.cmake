0210de8777940f8dacff053736bc714a5403a595
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-07-18 21:59:40 +0200

    my template multithread

---
+++ template_multithread/build/CMakeFiles/TemplateMultithread.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplateMultithread.dir/src/main.cpp.o"
+  "CMakeFiles/TemplateMultithread.dir/src/main.cpp.o.d"
+  "TemplateMultithread"
+  "TemplateMultithread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplateMultithread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ template_multithread/build/CMakeFiles/TemplateMultithread.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TemplateMultithread.
+# This may be replaced when dependencies are built.
